c0cdffc179c6e514aa8a5fffff7025a543de7af7
jepsoniada <patryk.pelczar@gmail.com>
2025-08-08 02:42:27 +0200

    reindent

--- modules/config.el
+++ modules/config.el
@@ -426,8 +426,8 @@ or go back to just one window (by deleting all but the selected window)."
   :defer t)
 
 (use-package transient
-  :bind (:map transient-map
-              ("<escape>" . transient-quit-all)))
+  :bind ( :map transient-map
+          ("<escape>" . transient-quit-all)))
 
 (use-package expand-region
   :bind (("M-h" . er/mark-outside-quotes)))
